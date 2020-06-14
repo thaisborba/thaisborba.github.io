8b8a46a6ca62a230f18f0b3b7252f6794a74fbf0
Marcelo Belkiman <marcelobelkiman@gmail.com>
2020-06-14 15:28:41 -0300

    new colors

--- js/language.js
+++ js/language.js
@@ -1,7 +1,7 @@
 var language = {
     ptbr: {
         about: {
-            'title': 'Quem sou?',
+            'title': 'QUEM SOU',
             'content-1': 'Maquiadora profissional, artista visual e empreendedora em série.',
             'content-2': 'Foi pela vontade de trabalhar com arte e beleza, que Thais Borba decidiu seguir carreira no mundo da maquiagem. Fugindo do convencional e seguindo uma linha de maquiagem mais artística, foi a forma com que Thais encontrou para expressar o seu processo criativo.',
             'content-3': 'Já atua como maquiadora profissional há 5 anos. Especializou-se em Maquiagem Social, Editorial e Artística. Possui diversos cursos de especialização na área com profissionais renomados como Brigitte Calegari..',
